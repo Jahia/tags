c417b5826972cfe60751f11ab86050e1605dc0f0
Francois Pral <fpral@jahia.com>
2020-04-06 15:23:40 +0200

    BACKLOG-13102: added icon for tag manager entry in nav

--- src/main/resources/javascript/apps/tagmanager.js
+++ src/main/resources/javascript/apps/tagmanager.js
@@ -3,7 +3,7 @@ window.jahia.i18n.loadNamespaces('tags');
 window.jahia.uiExtender.registry.add('adminRoute', 'tagsmanager', {
     targets: ['jcontent:50'],
     label: 'tags:label.title',
-    icon: null,
+    icon: window.jahia.moonstone.toIconComponent('CollectionsBookmark'),
     isSelectable: true,
     requiredPermission: 'tagManager',
     requireModuleInstalledOnSite: 'tags',
