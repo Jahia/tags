1809c1f1028dd4c08a9b63e64b2cce76d22dd17e
Geofrey Flores <gflores@jahia.com>
2024-09-25 12:03:56 -0400

    Revert changes to old js scripts

--- src/main/resources/javascript/tagUsages.js
+++ src/main/resources/javascript/tagUsages.js
@@ -1,14 +1,14 @@
-function callWorkInProgress() {
-    if ($.browser.msie == true) {
-        $.blockUI({css: {
+function callWorkInProgress(){
+    if($.browser.msie == true){
+        $.blockUI({ css: {
             border: 'none',
             padding: '15px',
             backgroundColor: '#000',
             '-webkit-border-radius': '10px',
             '-moz-border-radius': '10px',
-            opacity: 0.5,
+            opacity: .5,
             color: '#fff'
-        }, message: jsVarMap.i18nWaiting});
+        }, message: jsVarMap.i18nWaiting });
     } else {
         workInProgress(jsVarMap.i18nWaiting);
     }
@@ -16,46 +16,46 @@ function callWorkInProgress() {
 
 function backToTagManager() {
     callWorkInProgress();
-    $('#eventInput').attr('name', '_eventId_backToTagsList');
-    $('#formTagManagement').submit();
+    $("#eventInput").attr("name", "_eventId_backToTagsList");
+    $("#formTagManagement").submit();
 }
 
 function bbRenameTag(nodeID) {
     bootbox.dialog({
-        title: '<h4>' + jsVarMap.labelRename + ' : ' + $('#selectedTag').val() + '</h4>',
-        message: '<p>' + jsVarMap.labelTagNewName +
-            '</p><input id=\'renameTag\' class=\'typeahead\' type=\'text\' value=\'' + _.escape($('#selectedTag').val()) + '\'>' +
-            '<script>' +
-                '$(\'.modal-body\').css(\'overflow\', \'visible\');' +
-                '$(\'#renameTag\').keyup(function() {' +
-                    'if ($(\'#renameTag\').val() != \'\') {' +
-                        '$(\'.renameButton\').removeAttr(\'disabled\');' +
-                    '} else {' +
-                        '$(\'.renameButton\').attr(\'disabled\', \'disabled\');' +
-                    '}' +
-                '});' +
-                '$(\'#renameTag\').typeahead(null, {' +
-                    'source: tagsSuggester.ttAdapter()' +
-                '});' +
-            '</script>' +
-            '<br /><br /><p>' +
+        title: "<h4>" + jsVarMap.labelRename + " : " + $('#selectedTag').val() + "</h4>",
+        message: "<p>" + jsVarMap.labelTagNewName +
+            "</p><input id='renameTag' class='typeahead' type='text' value='" + _.escape($('#selectedTag').val()) + "'>" +
+            "<script>" +
+                "$('.modal-body').css('overflow', 'visible');" +
+                "$('#renameTag').keyup(function() {" +
+                    "if ($('#renameTag').val() != '') {" +
+                        "$('.renameButton').removeAttr('disabled');" +
+                    "} else {" +
+                        "$('.renameButton').attr('disabled', 'disabled');" +
+                    "}" +
+                "});" +
+                "$('#renameTag').typeahead(null, {" +
+                    "source: tagsSuggester.ttAdapter()" +
+                "});" +
+            "</script>" +
+            "<br /><br /><p>" +
             jsVarMap.modalRename +
-            '</p>',
+            "</p>",
         buttons: {
             danger: {
                 label: jsVarMap.labelCancel,
-                className: 'btn-default',
-                callback: function () {}
+                className: "btn-default",
+                callback: function() {}
             },
             success: {
                 label: jsVarMap.labelRename,
-                className: 'btn-primary btn-raised renameButton',
-                callback: function () {
+                className: "btn-primary btn-raised renameButton",
+                callback: function() {
                     callWorkInProgress();
-                    $('#eventInput').attr('name', '_eventId_renameTagOnNode');
-                    $('#nodeToUpdateId').val(nodeID);
-                    $('#tagNewName').val($('#renameTag').val());
-                    $('#formTagManagement').submit();
+                    $("#eventInput").attr("name", "_eventId_renameTagOnNode");
+                    $("#nodeToUpdateId").val(nodeID);
+                    $("#tagNewName").val($("#renameTag").val());
+                    $("#formTagManagement").submit();
                 }
             }
         }
@@ -64,22 +64,22 @@ function bbRenameTag(nodeID) {
 
 function bbDeleteTag(nodeID) {
     bootbox.dialog({
-        title: '<h4>' + jsVarMap.labelDelete + ' : ' + $('#selectedTag').val() + '</h4>',
-        message: '<p>' + jsVarMap.modalDelete + '</p>',
+        title: "<h4>" + jsVarMap.labelDelete + " : " + $('#selectedTag').val() + "</h4>",
+        message: "<p>" + jsVarMap.modalDelete + "</p>",
         buttons: {
             danger: {
                 label: jsVarMap.labelCancel,
-                className: 'btn-default',
-                callback: function () {}
+                className: "btn-default",
+                callback: function() {}
             },
             success: {
                 label: jsVarMap.labelDelete,
-                className: 'btn-danger btn-raised',
-                callback: function () {
+                className: "btn-danger btn-raised",
+                callback: function() {
                     callWorkInProgress();
-                    $('#eventInput').attr('name', '_eventId_deleteTagOnNode');
-                    $('#nodeToUpdateId').val(nodeID);
-                    $('#formTagManagement').submit();
+                    $("#eventInput").attr("name", "_eventId_deleteTagOnNode");
+                    $("#nodeToUpdateId").val(nodeID);
+                    $("#formTagManagement").submit();
                 }
             }
         }
@@ -87,32 +87,32 @@ function bbDeleteTag(nodeID) {
 }
 
 function attachRenameAndDeleteListeners() {
-    var renameButtons = document.getElementsByClassName('renameTagButton');
+    var renameButtons = document.getElementsByClassName("renameTagButton");
     for (var i = 0; i < renameButtons.length; i++) {
-        renameButtons[i].addEventListener('click', function (e) {
-            bbRenameTag(e.currentTarget.id.replace('rename_', ''));
+        renameButtons[i].addEventListener("click", function(e) {
+            bbRenameTag(e.currentTarget.id.replace("rename_", ""));
         });
     }
 
-    var deleteButtons = document.getElementsByClassName('deleteTagButton');
+    var deleteButtons = document.getElementsByClassName("deleteTagButton");
     for (var i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener('click', function (e) {
-            bbDeleteTag(e.currentTarget.id.replace('delete_', ''));
+        deleteButtons[i].addEventListener("click", function(e) {
+            bbDeleteTag(e.currentTarget.id.replace("delete_", ""));
         });
     }
 }
 
 function attachViewUsagesListeners() {
-    var usageButtons = document.getElementsByClassName('viewUsageButton');
+    var usageButtons = document.getElementsByClassName("viewUsageButton");
     for (var i = 0; i < usageButtons.length; i++) {
-        usageButtons[i].addEventListener('click', function (e) {
-            viewUsages(e.currentTarget.id.replace('usage_', ''));
+        usageButtons[i].addEventListener("click", function(e) {
+            viewUsages(e.currentTarget.id.replace("usage_", ""));
         });
     }
 }
 
 function attachWorkspaceSwitch() {
-    document.getElementById('wsSwitch').addEventListener('click', function () {
+    document.getElementById("wsSwitch").addEventListener("click", function () {
         switchWorkspace();
-    });
+    })
 }
